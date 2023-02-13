64ae20c7c5fe7d5660175c8255b428ca2ca8fd1c
Katsu <karmaelin8@gmail.com>
2023-02-13 08:32:44 +0300

    characters page

--- src/Core/RickAndMortyWiki.Application/Helpers/Mapping/CharacterMapper.cs
+++ src/Core/RickAndMortyWiki.Application/Helpers/Mapping/CharacterMapper.cs
@@ -8,7 +8,7 @@ public static class CharacterMapper
     public static Page<Character> ResponseToPage(GraphQlMultipleResponse<Character> response) =>
         new()
         {
-            Total = response.Response.Info.Count ?? 0,
+            Total = response.Response.Info.Pages ?? 0,
             Prev = response.Response.Info.Prev,
             Next = response.Response.Info.Next,
             Units = response.Response.Results
---
+++ src/Presentation/RickAndMortyWiki/Pages/Characters.razor.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Components;
+using RickAndMortyWiki.Application.Services.Interfaces;
+using RickAndMortyWiki.Domain.GraphQl.Helpers;
+using RickAndMortyWiki.Domain.Models;
+
+namespace RickAndMortyWiki.Pages;
+
+public partial class Characters
+{
+    [Inject] private ICharacterService CharacterService { get; set; } = null!;
+
+    private Page<Character> _page = null!;
+    private List<Character> _characters = null!;
+    private Filter _filter = new();
+    private bool _showFilters;
+    private int _currentPage = 1;
+    private int _maxPage = 1;
+    private List<int> _displayedPages = new();
+
+    protected override async Task OnInitializedAsync()
+    {
+        await UpdatePage();
+    }
+
+    private async Task UpdatePage()
+    {
+        _characters = null!;
+        StateHasChanged();
+        _page = await CharacterService.GetPage(_currentPage, _filter);
+        _maxPage = _page.Total;
+        _characters = _page.Units;
+        GeneratePagination();
+        StateHasChanged();
+    }
+
+    private async Task Search() => await UpdatePage();
+    
+    private async Task Reset()
+    {
+        _filter = new Filter();
+        await UpdatePage();
+    }
+
+    private async Task NavigateOnPage(string page)
+    {
+        switch (page)
+        {
+            case "prev":
+                _currentPage--;
+                await UpdatePage();
+                break;
+            case "next":
+                _currentPage++;
+                await UpdatePage();
+                break;
+            default:
+                var num = int.Parse(page);
+                if (_currentPage == num) break;
+                _currentPage = num;
+                await UpdatePage();
+                break;
+        }
+    }
+
+    private void GeneratePagination()
+    {
+        if (_maxPage <= 1) return;
+        
+        if (_maxPage <= 3) _displayedPages = Enumerable.Range(1, _maxPage).ToList();
+        else if (_maxPage - _currentPage < 3) _displayedPages = Enumerable.Range(_maxPage - 3, 3).ToList();
+        else if (_currentPage < 3) _displayedPages = Enumerable.Range(_currentPage, 3).ToList();
+        else _displayedPages = Enumerable.Range(_currentPage - 1, 3).ToList();
+        if(!_displayedPages.Contains(1)) _displayedPages.Insert(0, 1);
+        if(!_displayedPages.Contains(_maxPage)) _displayedPages.Add(_maxPage);
+    }
+}
