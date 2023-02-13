8664e03727b89e3f4351270b57a2063126a6ff72
Katsu <karmaelin8@gmail.com>
2023-02-13 15:16:55 +0300

    smth wrong with gh pages :(

--- src/Presentation/RickAndMortyWiki/Program.cs
+++ src/Presentation/RickAndMortyWiki/Program.cs
@@ -7,6 +7,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
 builder.Services.AddApplication();
 
 await builder.Build().RunAsync();
